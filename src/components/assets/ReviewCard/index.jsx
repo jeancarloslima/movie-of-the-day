f4944f2ef57f11699667c888f406c67975adc380
Jean Lima <jcoliveiralima19@gmail.com>
2026-04-23 14:49:04 -0300

    Ajustando layout da HomePage para telas pequenas

--- src/components/assets/ReviewCard/index.jsx
+++ src/components/assets/ReviewCard/index.jsx
@@ -11,20 +11,20 @@ export default function ReviewCard({
 }) {
   return (
     <article
-      className={`w-full max-w-[700px] cursor-pointer border-b border-gray-300 mb-6 px-4 md:px-0 ${index !== undefined ? "sm:border-b-0 sm:mb-0 sm:px-0" : ""} ${index === 3 || index === 4 ? "sm:hidden" : index === 0 ? "sm:pr-4 md:pr-4 sm:row-span-2 sm:border-r border-gray-300 h-full" : "sm:h-full"}`}
+      className={`w-full max-w-[832px] cursor-pointer border-b border-gray-300 mb-6 md:px-0 ${index !== undefined ? "sm:border-b-0 sm:mb-0 sm:px-0" : ""} ${index === 3 || index === 4 ? "sm:hidden px-4" : index === 0 ? "px-0 sm:px-4 sm:pr-4 md:pr-4 sm:row-span-2 sm:border-r border-gray-300 h-full" : "sm:h-full px-4"}`}
     >
-      <Link className={`w-full h-full grid grid-cols-[3fr_1fr] gap-2 pb-6 ${index !== undefined ? "sm:grid-cols-1 sm:pb-0" : ""} ${index === 0 ? "sm:gap-12" : ""}`} to={`/review/${id}`}>
+      <Link className={`w-full h-full grid gap-2 pb-6 ${index !== undefined ? "sm:grid-cols-1 sm:pb-0" : ""} ${index === 0 ? "grid-cols-1 sm:gap-12" : "grid-cols-[3fr_1fr]"}`} to={`/review/${id}`}>
         <div className={`${index === 0 ? "sm:text-center sm:max-w-[360px] sm:mx-auto" : ""}`}>
           <h2
-            className={`font-semibold text-lg ${index === 0 ? "sm:text-2xl" : ""}`}
+            className={`font-semibold ${index === 0 ? "text-2xl px-4 sm:px-0" : "text-lg"}`}
           >{`${id}: ${title}`}</h2>
-          <p className={`text-justify ${index !== undefined ? "sm:truncate" : ""} ${index === 0 ? "sm:text-center sm:text-lg sm:py-4" : ""}`}>{summary}</p>
+          <p className={`text-justify ${index !== undefined ? "sm:truncate" : ""} ${index === 0 ? "sm:text-center sm:text-lg py-2 px-4 sm:px-0 sm:py-4" : ""}`}>{summary}</p>
           <span
-            className={`text-sm text-zinc-600`}
+            className={`text-sm text-zinc-600 ${index === 0 ? "px-4 sm:px-0" : ""}`}
           >{`${date} * ${author}`}</span>
         </div>
         <img
-          className={`w-full rounded object-cover h-[72px] ${index !== undefined ? "sm:order-first sm:w-full sm:h-[100%]" : "sm:h-[100px]"}`}
+          className={`w-full rounded object-cover h-[72px] ${index === 0 ? "min-h-[230px] order-first rounded-none" : ""} ${index !== undefined ? "sm:order-first sm:w-full sm:h-[100%]" : "sm:h-[100px]"}`}
           src={imageURL}
           alt={`${title} image`}
         />
--- src/components/pages/HomePage/index.jsx
+++ src/components/pages/HomePage/index.jsx
@@ -20,7 +20,7 @@ export default function HomePage({ reviews, setTelaAtiva }) {
   return (
     <div className="mx-auto w-full max-w-[832px]">
       <div>
-        <ul className="flex flex-col justify-center items-center sm:h-[600px] pt-8 pb-2 sm:grid sm:grid-cols-[4fr_2fr] sm:grid-rows-2 sm:gap-4 sm:pb-6 sm:px-4 sm:border-b border-gray-300">
+        <ul className="flex flex-col justify-center items-center sm:h-[600px] pb-8 sm:pt-8 sm:grid sm:grid-cols-[4fr_2fr] sm:grid-rows-2 sm:gap-4 sm:pb-6 sm:px-4 sm:border-b border-gray-300">
           {secondPart.map((item, index) => (
             <ReviewCard
               index={index}
@@ -35,8 +35,8 @@ export default function HomePage({ reviews, setTelaAtiva }) {
           ))}
         </ul>
       </div>
-      <div>
-        <h2 className="text-xl font-semibold pl-4 pt-4">Recent Posts</h2>
+      <div className="w-full max-w-[832px] mx-auto">
+        <h2 className="text-xl font-semibold pt-4">Recent Posts</h2>
         <ul className="flex flex-col justify-center items-center pt-8 pb-2">
           {firstPart.map((item) => (
             <ReviewCard
@@ -52,7 +52,7 @@ export default function HomePage({ reviews, setTelaAtiva }) {
         </ul>
         <button
           onClick={openArchive}
-          className="cursor-pointer rounded-lg bg-gray-200 py-2 px-4 mb-8 ml-4 hover:bg-blue-500 hover:text-white"
+          className="cursor-pointer rounded-lg bg-gray-200 py-2 px-4 mb-8 hover:bg-blue-500 hover:text-white"
         >
           See all &#62;
         </button>
