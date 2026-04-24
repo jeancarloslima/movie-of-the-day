6a03897c16edeb2b63c917c93aba249bc4f5cedb
Jean Lima <jcoliveiralima19@gmail.com>
2026-04-24 12:10:44 -0300

    Ajustando espaçamento da HomePage

--- src/components/pages/HomePage/index.jsx
+++ src/components/pages/HomePage/index.jsx
@@ -4,8 +4,6 @@ import ReviewCard from "../../assets/ReviewCard";
 export default function HomePage({ reviews, setTelaAtiva }) {
   const navigate = useNavigate();
   const filteredReviews = reviews.slice(-11);
-  const firstPart = filteredReviews.slice(0, 6).reverse();
-  const secondPart = filteredReviews.slice(6).reverse();
 
   const openArchive = () => {
     navigate("/archive");
@@ -21,7 +19,7 @@ export default function HomePage({ reviews, setTelaAtiva }) {
     <div className="mx-auto w-full max-w-[770px]">
       <div>
         <ul className="flex flex-col justify-center items-center sm:h-[600px] pb-8 sm:pt-8 sm:grid sm:grid-cols-[4fr_2fr] sm:grid-rows-2 sm:gap-4 sm:pb-6 sm:px-4 sm:border-b border-gray-300">
-          {secondPart.map((item, index) => (
+          {filteredReviews.slice(6).reverse().map((item, index) => (
             <ReviewCard
               index={index}
               key={item.id}
@@ -36,9 +34,9 @@ export default function HomePage({ reviews, setTelaAtiva }) {
         </ul>
       </div>
       <div className="w-full max-w-[770px] mx-auto">
-        <h2 className="text-xl font-semibold pt-4">Recent Posts</h2>
+        <h2 className="text-xl font-semibold pt-4 pl-4 md:pl-0">Recent Posts</h2>
         <ul className="flex flex-col justify-center items-center pt-8 pb-2">
-          {firstPart.map((item) => (
+          {filteredReviews.slice(0,6).reverse().map((item) => (
             <ReviewCard
               key={item.id}
               id={item.id}
@@ -52,7 +50,7 @@ export default function HomePage({ reviews, setTelaAtiva }) {
         </ul>
         <button
           onClick={openArchive}
-          className="cursor-pointer rounded-lg bg-gray-200 py-2 px-4 mb-8 hover:bg-blue-500 hover:text-white"
+          className="cursor-pointer rounded-lg bg-gray-200 py-2 px-4 mb-8 ml-4 md:ml-0 hover:bg-blue-500 hover:text-white"
         >
           See all &#62;
         </button>
